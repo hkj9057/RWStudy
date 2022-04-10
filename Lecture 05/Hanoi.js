5ba13e377c6563f5648379c53926dc2798eb9dd8
hkj9057@office.yuhan.ac.kr <hkj9057@office.yuhan.ac.kr>
2022-04-10 23:06:00 +0900

    [ES06]

---
+++ Lecture 05/Hanoi.js
@@ -0,0 +1,13 @@
+var Rsq = document.getElementById('Rsq');
+var Ysq = document.getElementById('Ysq');
+var Gsq = document.getElementById('Gsq');
+var Bsq = document.getElementById('Bsq');
+var Psq = document.getElementById('Psq');
+var Lsq = document.getElementById('Lsq');
+
+function Click()
+{
+    var hidden = document.getElementsByClassName("sq")[0];
+
+    hidden.style.visibility = "visible";
+}
