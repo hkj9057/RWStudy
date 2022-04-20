750e7ac278c374c13b02c95ad7971dd91683babf
unknown <hkj9057>
2022-04-20 15:55:02 +0900

    [ES08]

--- Lecture 05/Hanoi.js
+++ Lecture 05/Hanoi.js
@@ -1,13 +1,47 @@
-var Rsq = document.getElementById('Rsq');
-var Ysq = document.getElementById('Ysq');
-var Gsq = document.getElementById('Gsq');
-var Bsq = document.getElementById('Bsq');
-var Psq = document.getElementById('Psq');
-var Lsq = document.getElementById('Lsq');
+var arraydiv1 = new Array();
+var arraydiv2 = new Array();
+var arraydiv3 = new Array();
+var arraydiv4 = document.getElementById("arraydiv4");
+var arraydiv5 = document.getElementById('arraydiv5');
+var arraydiv6 = document.getElementById('arraydiv6');
+var i;
 
-function Click()
+arraydiv1.push(document.createTextNode("🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥"));
+arraydiv1.push(document.createTextNode("🟨🟨🟨🟨🟨🟨🟨🟨🟨"));
+arraydiv1.push(document.createTextNode("🟩🟩🟩🟩🟩🟩🟩"));
+arraydiv1.push(document.createTextNode("🟦🟦🟦🟦🟦"));
+arraydiv1.push(document.createTextNode("🟪🟪🟪"));
+arraydiv1.push(document.createTextNode("⬛"));
+
+function basicrender(a, b)
 {
-    var hidden = document.getElementsByClassName("sq")[0];
-
-    hidden.style.visibility = "visible";
+    a.appendChild(arraydiv1[i]); 
+    b.appendChild(document.createElement('br'));
+        
 }
+
+for (i = 0; i < 6; i++) {
+    basicrender(arraydiv1)
+    
+}
+arraydiv4.appendChild(arraydiv1[0]); 
+    arraydiv4.appendChild(document.createElement('br'));
+    arraydiv4.appendChild(arraydiv1[1]); 
+    arraydiv4.appendChild(document.createElement('br'));
+    arraydiv4.appendChild(arraydiv1[2]); 
+    arraydiv4.appendChild(document.createElement('br'));
+    arraydiv4.appendChild(arraydiv1[3]); 
+    arraydiv4.appendChild(document.createElement('br'));
+    arraydiv4.appendChild(arraydiv1[4]); 
+    arraydiv4.appendChild(document.createElement('br'));
+    arraydiv4.appendChild(arraydiv1[5]); 
+   
+
+
+function firstboxmove()
+{
+    arraydiv2.push(arraydiv1[5]);
+    arraydiv5.appendChild(arraydiv2[0]);
+}
+
+
